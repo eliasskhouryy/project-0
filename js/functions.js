940c6d1a297975dcc4243a0bd5402cd9fc4ec01d
Elias Khoury <eliaskhouryy@hotmail.com>
2022-07-05 21:11:42 +1000

    update

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,5 @@
 const cross = `<img style="max-width: 98%;" src="img/cross.png" alt="cross">`;
-const circle = `<img style="max-width: 100%;" src="img/circle.png" alt="cross">`;
+const circle = `<img style="max-width: 100%;" src="img/circle.png" alt="cricle">`;
 const clickSound = new Audio(`sounds/misClick.wav`);
 const misClickSound = new Audio(`sounds/newClick.wav`);
 
@@ -19,72 +19,74 @@ const clearBox = function(){
 
 const win = function() {
     if ($(`#box1`).html() == cross && $(`#box2`).html() == cross && $(`#box3`).html() == cross) {
-        alert(`PLAYER 1 wins!`);
+        setTimeout(function() {alert(`PLAYER 1 wins!`); clearBox();}
+        , 300);
         winCounter.player1++;
-        $(`.player1`).html(`${winCounter.player1}`); clearBox();
+        $(`.player1`).html(`${winCounter.player1}`);     
     } else if ($(`#box1`).html() == circle && $(`#box2`).html() == circle && $(`#box3`).html() == circle) {
+        setTimeout(function() {alert(`PLAYER 2 wins!`); clearBox();}, 300)
         winCounter.player2++;
-        alert(`PLAYER 2 wins!`)
-        $(`.player2`).html(`${winCounter.player2}`); clearBox();
+        $(`.player2`).html(`${winCounter.player2}`); 
     } else if ($(`#box4`).html() == cross && $(`#box5`).html() == cross && $(`#box6`).html() == cross) {
-        alert(`PLAYER 1 wins!`);
+        setTimeout(function() {alert(`PLAYER 1 wins!`); clearBox();}, 300);
         winCounter.player1++;
-        $(`.player1`).html(`${winCounter.player1}`); clearBox();
+        $(`.player1`).html(`${winCounter.player1}`); 
     } else if ($(`#box4`).html() == circle && $(`#box5`).html() == circle && $(`#box6`).html() == circle) {
+        setTimeout(function() {alert(`PLAYER 2 wins!`); clearBox();}, 300);
         winCounter.player2++;
-        alert(`PLAYER 2 wins!`);
-        $(`.player2`).html(`${winCounter.player2}`); clearBox();
+        $(`.player2`).html(`${winCounter.player2}`); 
     } else if ($(`#box7`).html() == cross && $(`#box8`).html() == cross && $(`#box9`).html() == cross) {
-        alert(`PLAYER 1 wins!`);
+        setTimeout(function() {alert(`PLAYER 1 wins!`); clearBox();}, 300);
         winCounter.player1++;
-        $(`.player1`).html(`${winCounter.player1}`); clearBox();
+        $(`.player1`).html(`${winCounter.player1}`); 
     } else if ($(`#box7`).html() == circle && $(`#box8`).html() == circle && $(`#box9`).html() == circle) {
+        setTimeout(function() {alert(`PLAYER 2 wins!`); clearBox();}, 300)
         winCounter.player2++;
-        alert(`PLAYER 2 wins!`)
-        $(`.player2`).html(`${winCounter.player2}`); clearBox();
+        $(`.player2`).html(`${winCounter.player2}`); 
     } else if ($(`#box1`).html() == cross && $(`#box4`).html() == cross && $(`#box7`).html() == cross) {
-        alert(`PLAYER 1 wins!`);
+        setTimeout(function() {alert(`PLAYER 1 wins!`); clearBox();}, 300);
         winCounter.player1++;
-        $(`.player1`).html(`${winCounter.player1}`); clearBox();
+        $(`.player1`).html(`${winCounter.player1}`); 
     } else if ($(`#box1`).html() == circle && $(`#box4`).html() == circle && $(`#box7`).html() == circle) {
+        setTimeout(function() {alert(`PLAYER 2 wins!`); clearBox();}, 300)
         winCounter.player2++;
-        alert(`PLAYER 2 wins!`)
-        $(`.player2`).html(`${winCounter.player2}`); clearBox();
+        $(`.player2`).html(`${winCounter.player2}`); 
     } else if ($(`#box2`).html() == cross && $(`#box5`).html() == cross && $(`#box8`).html() == cross) {
-        alert(`PLAYER 1 wins!`);
+        setTimeout(function() {alert(`PLAYER 1 wins!`); clearBox();}, 300);
         winCounter.player1++;
-        $(`.player1`).html(`${winCounter.player1}`); clearBox();
+        $(`.player1`).html(`${winCounter.player1}`); 
     } else if ($(`#box2`).html() == circle && $(`#box5`).html() == circle && $(`#box8`).html() == circle) {
+        setTimeout(function() {alert(`PLAYER 2 wins!`); clearBox();}, 300)
         winCounter.player2++;
-        alert(`PLAYER 2 wins!`)
-        $(`.player2`).html(`${winCounter.player2}`); clearBox();
+        $(`.player2`).html(`${winCounter.player2}`); 
     } else if ($(`#box3`).html() == cross && $(`#box6`).html() == cross && $(`#box9`).html() == cross) {
-        alert(`PLAYER 1 wins!`);
+        setTimeout(function() {alert(`PLAYER 1 wins!`); clearBox();}, 300);
         winCounter.player1++;
-        $(`.player1`).html(`${winCounter.player1}`); clearBox();
+        $(`.player1`).html(`${winCounter.player1}`); 
     } else if ($(`#box3`).html() == circle && $(`#box6`).html() == circle && $(`#box9`).html() == circle) {
+        setTimeout(function() {alert(`PLAYER 2 wins!`); clearBox();}, 300)
         winCounter.player2++;
-        alert(`PLAYER 2 wins!`)
-        $(`.player2`).html(`${winCounter.player2}`); clearBox();
+        $(`.player2`).html(`${winCounter.player2}`); 
     } else if ($(`#box1`).html() == cross && $(`#box5`).html() == cross && $(`#box9`).html() == cross) {
-        alert(`PLAYER 1 wins!`);
+        setTimeout(function() {alert(`PLAYER 1 wins!`); clearBox();}, 300);
         winCounter.player1++;
-        $(`.player1`).html(`${winCounter.player1}`); clearBox();
+        $(`.player1`).html(`${winCounter.player1}`); 
     } else if ($(`#box1`).html() == circle && $(`#box5`).html() == circle && $(`#box9`).html() == circle) {
-        winCounter.player2++;
-        alert(`PLAYER 2 wins!`)
-        $(`.player2`).html(`${winCounter.player2}`); clearBox();
+        setTimeout(function() {alert(`PLAYER 2 wins!`); clearBox();}, 300)
+        winCounter.player2++;        
+        $(`.player2`).html(`${winCounter.player2}`); 
     } else if ($(`#box3`).html() == cross && $(`#box5`).html() == cross && $(`#box7`).html() == cross) {
-        alert(`PLAYER 1 wins!`);
+        setTimeout(function() {alert(`PLAYER 1 wins!`); clearBox();}, 300);
         winCounter.player1++;
-        $(`.player1`).html(`${winCounter.player1}`); clearBox();
+        $(`.player1`).html(`${winCounter.player1}`); 
     } else if ($(`#box3`).html() == circle && $(`#box5`).html() == circle && $(`#box7`).html() == circle) {
+         setTimeout(function() {alert(`PLAYER 2 wins!`); clearBox();}, 300)
         winCounter.player2++;
-        alert(`PLAYER 2 wins!`)
-        $(`.player2`).html(`${winCounter.player2}`); clearBox();
+        $(`.player2`).html(`${winCounter.player2}`); 
     } else if (turnCount >= 9) {
-        alert(`draw`);
-        clearBox();
+        setTimeout(function() {alert(`draw`);
+        clearBox();}, 300);
+        
     }
 }
 
--- js/interact.js
+++ js/interact.js
@@ -1,21 +1,42 @@
 
 
+$(`#pressToPlay`).on(`click`, function(){
+    $(`.initial`).addClass(`visibilityHidden`);
+    $(`.container`).removeClass(`visibilityHidden`);
+})
+
+$(`.gridItem`).mouseenter(function() {
+
+    if (turnCount % 2 === 0) {
+        $(this).addClass(`crossBackground`);
+    } else {
+        $(this).addClass(`circleBackground`);
+    }   
+}).mouseleave(function(){
+    if (turnCount % 2 === 0) {
+        $(this).removeClass(`crossBackground`);
+    } else {
+        $(this).removeClass(`circleBackground`);
+    }   
+})
+
+
 $(`.gridItem`).on(`click`, function() {
     if ($(this).html() != '') {
         misClickSound.play();
         return;
     }
     if (turnCount % 2 === 0) {
-        clickSound.play()
+        clickSound.play(0.2)
         $(this).html(cross); 
         turnCount++;
-        $(`.turn`).html(`2`)
+        $(`.turn`).html(`O`)
         win();
     } else {
-        clickSound.play()
+        clickSound.play(0.2)
         $(this).html(circle); 
         turnCount++;
-        $(`.turn`).html(`1`)
+        $(`.turn`).html(`X`)
         win();
     }    
 });
